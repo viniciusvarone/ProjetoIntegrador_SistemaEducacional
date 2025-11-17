d649e8465a8db828d91e8c5243b2bc45e91afe5b
viniciusvarone <139243276+viniciusvarone@users.noreply.github.com>
2025-11-17 15:26:50 -0300

    Update modelo_banco.sql

--- modelo_dados/modelo_banco.sql
+++ modelo_dados/modelo_banco.sql
@@ -1,28 +1,6 @@
 CREATE DATABASE Sistema_Universidade;
 USE Sistema_Universidade;
 
-CREATE TABLE IF NOT EXISTS Pessoa_fisica (
-	id INT PRIMARY KEY NOT NULL,
-	cpf CHAR(11) NOT NULL,
-    rg VARCHAR(15) NOT NULL,
-    dataNascimento DATE,
-	FOREIGN KEY (id) REFERENCES Pessoa(id)
-); 
-
-CREATE TABLE IF NOT EXISTS Aluno (
-	id INT PRIMARY KEY,
-	matricula VARCHAR(70), 
-    curso VARCHAR(70),
-    FOREIGN KEY (id) REFERENCES Pessoa_fisica(id)
-);
-
-CREATE TABLE IF NOT EXISTS Professor (
-	id INT PRIMARY KEY,
-    email_corporativo VARCHAR (70),
-    disciplina VARCHAR (70),
-    matricula VARCHAR (70),
-    FOREIGN KEY (id) REFERENCES Pessoa_fisica(id)
-);
 
 CREATE TABLE IF NOT EXISTS Pessoa (
     id INT PRIMARY KEY,
@@ -31,20 +9,42 @@ CREATE TABLE IF NOT EXISTS Pessoa (
     telefone VARCHAR(20)
 );
 
-CREATE TABLE IF NOT EXISTS Pessoa_juridica (
-	id INT PRIMARY KEY NOT NULL,
-	cnpj VARCHAR (70) NOT NULL,
-    endereco VARCHAR (100),
+CREATE TABLE IF NOT EXISTS Pessoa_fisica (
+    id INT PRIMARY KEY,
+    cpf CHAR(11) NOT NULL UNIQUE,
+    rg VARCHAR(15) NOT NULL UNIQUE,
+    dataNascimento DATE,
     FOREIGN KEY (id) REFERENCES Pessoa(id)
 );
 
-CREATE TABLE IF NOT EXISTS Fornecedor (
-	id_fornecedor INT PRIMARY KEY NOT NULL,
-    data_contrato DATE, 
-    servico VARCHAR (100),
-    FOREIGN KEY (id_fornecedor) REFERENCES Pessoa_jurídica(id)
+CREATE TABLE IF NOT EXISTS Pessoa_juridica (
+    id INT PRIMARY KEY,
+    cnpj VARCHAR(14) NOT NULL UNIQUE,
+    endereco VARCHAR(100),
+    FOREIGN KEY (id) REFERENCES Pessoa(id)
 );
 
+CREATE TABLE IF NOT EXISTS Aluno (
+    id INT PRIMARY KEY,
+    matricula VARCHAR(70) UNIQUE,
+    curso VARCHAR(70),
+    FOREIGN KEY (id) REFERENCES Pessoa_fisica(id)
+);
+
+CREATE TABLE IF NOT EXISTS Professor (
+    id INT PRIMARY KEY,
+    email_corporativo VARCHAR(70),
+    disciplina VARCHAR(70),
+    matricula VARCHAR(70) UNIQUE,
+    FOREIGN KEY (id) REFERENCES Pessoa_fisica(id)
+);
+
+CREATE TABLE IF NOT EXISTS Fornecedor (
+    id INT PRIMARY KEY,
+    data_contrato DATE,
+    servico VARCHAR(100),
+    FOREIGN KEY (id) REFERENCES Pessoa_juridica(id)
+);
 
 
 
